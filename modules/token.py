6080d8a679c2a750e1ab6be276820be699828123
Pixel Orange <PixelOrange@users.noreply.github.com>
2014-08-22 03:23:16 -0500

    Update token.py

--- modules/token.py
+++ modules/token.py
@@ -12,8 +12,17 @@ import logging
 
 def start_increment(data,r,awardee):
   logging.debug("Starting Module: Token")
-  get_flair(data,r,awardee)
+  awardee_flair = get_flair(data,r,awardee)
+  set_flair(data,r,awardee,awardee_flair)
 
 def get_flair(data,r,awardee):
+  logging.debug("Getting the awardee's flair")
   awardee_flair = r.get_flair(data["running_subreddit"],awardee)
-  print(awardee_flair)
+  if awardee_flair["flair_text"] == None:
+    awardee_flair["flair_text"] = "1\u2206"
+    print awardee_flair["flair_text"]
+    return awardee_flair
+
+def set_flair(data,r,awardee,awardee_flair):
+  logging.debug("Setting the awardee's new flair")
+  r.set_flair(data["running_subreddit"],awardee,awardee_flair["flair_text"])
