a6d4674e761114b73b13740e5fbe490aa67b390d
Pixel Orange <PixelOrange@users.noreply.github.com>
2014-08-22 02:54:44 -0500

    Update token.py

--- modules/token.py
+++ modules/token.py
@@ -14,6 +14,6 @@ def start_increment(data,r,awardee):
   logging.debug("Starting Module: Token")
   get_flair(data,r,awardee)
 
-def get_flair(data,r,awardee)
+def get_flair(data,r,awardee):
   awardee_flair = r.get_flair(data["running_subreddit"],awardee)
   print(awardee_flair)
