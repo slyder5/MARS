33a8854965c6e8cbc0587b652ace9dcc179903fa
Pixel Orange <PixelOrange@users.noreply.github.com>
2014-08-22 03:34:07 -0500

    Update token.py

--- modules/token.py
+++ modules/token.py
@@ -19,7 +19,7 @@ def get_flair(data,r,awardee):
   logging.debug("Getting the awardee's flair")
   awardee_flair = r.get_flair(data["running_subreddit"],awardee)
   if awardee_flair["flair_text"] == None:
-    awardee_flair["flair_text"] = "1\u2206"
+    awardee_flair["flair_text"] = "1∆"
     print awardee_flair["flair_text"]
     return awardee_flair
 
