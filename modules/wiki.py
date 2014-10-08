416aaeb6927872e18b91734febb2958581f090ef
Pixel Orange <PixelOrange@users.noreply.github.com>
2014-10-08 04:20:04 -0500

    test

--- modules/wiki.py
+++ modules/wiki.py
@@ -1,1 +1,19 @@
 # encoding: utf-8
+
+# Comments #
+############
+
+# Import #
+##########
+
+import praw
+import logging
+
+# Functions #
+#############
+
+def start(data,r,token_comment,awarder,awardee,flair_count):
+  logging.debug("Starting Module: Wiki")
+  print awarder
+  print awardee
+  print flair_count
