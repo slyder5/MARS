64719ef5bf8f5f6ae6f7c70da5ee8a0ebd32c065
Pixel Orange <PixelOrange@users.noreply.github.com>
2014-10-08 03:21:33 -0500

    Update awardee flair

--- modules/token.py
+++ modules/token.py
@@ -19,10 +19,14 @@ def get_flair(data,r,awardee):
   logging.debug("Getting the awardee's flair")
   awardee_flair = r.get_flair(data["running_subreddit"],awardee)
   if awardee_flair["flair_text"] == None:
-    awardee_flair["flair_text"] = "1" + u'\2206'
+    awardee_flair["flair_text"] = "1∆",
     print awardee_flair["flair_text"]
     return awardee_flair
+  else:
+    print("This user has more than no flair.")
 
 def set_flair(data,r,awardee,awardee_flair):
   logging.debug("Setting the awardee's new flair")
   r.set_flair(data["running_subreddit"],awardee,awardee_flair["flair_text"])
+
+# EOF
