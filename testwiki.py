496ade6e6c2028ecf73ab3e34ebb7241c76cdf82
Pixel Orange <PixelOrange@users.noreply.github.com>
2014-10-10 01:51:42 -0500

    Update testwiki.py

--- testwiki.py
+++ testwiki.py
@@ -1,5 +1,8 @@
 # encoding: utf-8
 
+# MARS #
+########
+
 # Import #
 ##########
 
@@ -14,11 +17,10 @@ from logging.handlers import TimedRotatingFileHandler
 
 # Reads in configuration file
 data = config.read_json()
-link = "http://www.reddit.com/r/PixelOrange/comments/2iqwgh/mars_test_thread_october/cl4m2jt"
-r = account.start(data)
 
 # Logging #
 ###########
+
 consoleFormatter = logging.Formatter("%(asctime)s: %(message)s",datefmt="%I:%M:%S %p")
 fileFormatter = logging.Formatter("%(asctime)s %(levelname)s - %(message)s",datefmt="%I:%M:%S %p")
 rootLogger = logging.getLogger()
@@ -28,20 +30,22 @@ fileHandler.setFormatter(fileFormatter)
 rootLogger.addHandler(fileHandler)
 consoleHandler = logging.StreamHandler()
 if data["loglevel"] == "debug":
-  consoleHandler.setLevel(logging.DEBUG)
+	consoleHandler.setLevel(logging.DEBUG)
 elif data["loglevel"] == "info":
-  consoleHandler.setLevel(logging.INFO)
+	consoleHandler.setLevel(logging.INFO)
 else:
-  consoleHandler.setLevel(logging.WARNING)
+	consoleHandler.setLevel(logging.WARNING)
 consoleHandler.setFormatter(consoleFormatter)
 rootLogger.addHandler(consoleHandler)
 
 # Functions #
 #############
 
+r = account.start(data)
+link = "http://www.reddit.com/r/PixelOrange/comments/2iqwgh/mars_test_thread_october/cl4m2jt"
 token_comment = r.get_submission(link).comments
 for comment in token_comment:
-  print comment.author
-  wiki.start(data,r,comment,"Awarder","pixeltest1","2")
+	print comment.author
+	wiki.start(data,r,comment,"Awarder","pixeltest1","2")
 
 # EOF
