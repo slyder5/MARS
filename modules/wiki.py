bf225c7017bbf89a0fd530ac86b732d072585546
Pixel Orange <PixelOrange@users.noreply.github.com>
2014-10-10 03:35:00 -0500

    Update wiki.py

--- modules/wiki.py
+++ modules/wiki.py
@@ -18,8 +18,8 @@ from urllib2 import HTTPError
 
 def start(data,r,token_comment,awarder,awardee,flair_count):
   logging.debug("Starting Module: Wiki")
-  submission_url = token_comment.submission.permalink
   submission_title = token_comment.submission.title
+  submission_url = token_comment.submission.permalink
   today = datetime.date.today()
   try:
     user_wiki_page = r.get_wiki_page(data["running_subreddit"],"user/" + awardee)
@@ -35,22 +35,28 @@ def start(data,r,token_comment,awarder,awardee,flair_count):
     new_wiki_page(data,r,token_comment,awarder,awardee,flair_count)
 
 def update_wiki_page(data,r,token_comment,awarder,awardee,flair_count):
+  submission_title = token_comment.submission.title
+  submission_url = token_comment.submission.permalink
+  today = datetime.date.today()
   return
 
 def new_wiki_page(data,r,token_comment,awarder,awardee,flair_count):
-    if int(flair_count) < 2:
-      initial_text = "/u/%s has received %s delta for the following comments:\n\n" % (awardee,flair_count)
-    else:
-      initial_text = "/u/%s has received %s deltas for the following comments:\n\n" % (awardee,flair_count)
-    add_title = "| Submission | Delta Comment | Awarded By | Date |\n| --- | :-: | --- | --- |\n"
-    add_content = "|[%s](%s)|[Link](%s)|/u/%s|%s/%s/%s|\n" % (submission_title,submission_url,
-          token_comment.permalink + "?context=2",awarder,today.month,today.day,today.year)
-    full_update = initial_text + add_title + add_content
-    r.edit_wiki_page(data["running_subreddit"],"user/" + awardee,full_update,"Created user's delta history page.")
-    tracker_page = r.get_wiki_page(data["running_subreddit"],"index/delta_tracker")
-    logging.debug("Found existing tracker wiki page")
-    logging.debug("Could not find existing tracker wiki page")
-    add_link = "* /u/%s - [Delta List](/r/%s/wiki/%s)" % (awardee,data["running_subreddit"],awardee)
-    r.edit_wiki_page(data["running_subreddit"],"delta_tracker",add_link,"Updated tracker")
+  submission_title = token_comment.submission.title
+  submission_url = token_comment.submission.permalink
+  today = datetime.date.today()
+  if int(flair_count) < 2:
+    initial_text = "/u/%s has received %s delta for the following comments:\n\n" % (awardee,flair_count)
+  else:
+    initial_text = "/u/%s has received %s deltas for the following comments:\n\n" % (awardee,flair_count)
+  add_title = "| Submission | Delta Comment | Awarded By | Date |\n| --- | :-: | --- | --- |\n"
+  add_content = "|[%s](%s)|[Link](%s)|/u/%s|%s/%s/%s|\n" % (submission_title,submission_url,
+        token_comment.permalink + "?context=2",awarder,today.month,today.day,today.year)
+  full_update = initial_text + add_title + add_content
+  r.edit_wiki_page(data["running_subreddit"],"user/" + awardee,full_update,"Created user's delta history page.")
+  tracker_page = r.get_wiki_page(data["running_subreddit"],"index/delta_tracker")
+  logging.debug("Found existing tracker wiki page")
+  logging.debug("Could not find existing tracker wiki page")
+  add_link = "* /u/%s - [Delta List](/r/%s/wiki/%s)" % (awardee,data["running_subreddit"],awardee)
+  r.edit_wiki_page(data["running_subreddit"],"delta_tracker",add_link,"Updated tracker")
 
 # EOF
