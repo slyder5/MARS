141d655ca4a7667d3d147ced0b5ec443bf123fcd
Pixel Orange <PixelOrange@users.noreply.github.com>
2014-10-10 01:37:15 -0500

    Update testwiki.py

--- testwiki.py
+++ testwiki.py
@@ -16,10 +16,14 @@ data["running_username"] = "pixelsbot"
 data["running_password"] = "PixelOrange"
 data["m_account"] = "1"
 r = account.start(data)
+link = "http://www.reddit.com/r/PixelOrange/comments/2iqwgh/mars_test_thread_october/cl4m2jt"
 
 # Functions #
 #############
 
-wiki.start(data,r,"http://google.com","Awarder","pixeltest1","2")
+token_comment = r.get_submission(link).comments
+print token_comment.body
+print token_comment.author
+wiki.start(data,r,token_comment,"Awarder","pixeltest1","2")
 
 # EOF
