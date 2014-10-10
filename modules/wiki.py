4f3152d50c71009d6d56e31f65553d28f992a53a
Pixel Orange <PixelOrange@users.noreply.github.com>
2014-10-10 04:08:40 -0500

    Update wiki.py

--- modules/wiki.py
+++ modules/wiki.py
@@ -12,6 +12,7 @@ import time
 import datetime
 import calendar
 from urllib2 import HTTPError
+import re
 
 # Functions #
 #############
@@ -23,18 +24,28 @@ def start(data,r,token_comment,awarder,awardee,flair_count):
   today = datetime.date.today()
   try:
     user_wiki_page = r.get_wiki_page(data["running_subreddit"],"user/" + awardee)
-    print user_wiki_page
-    print user_wiki_page.content_md
     logging.debug("Found existing user wiki page")
-    found = True
+    user_found = True
   except Exception as e:
     if e.response.status_code == 404:
       logging.debug("Did not find existing user wiki page")
-      found = False
-  if found:
-    update_wiki_page(data,r,token_comment,awarder,awardee,flair_count)
+      user_found = False
+  if user_found:
+    update_wiki_page(data,r,token_comment,awarder,awardee,flair_count,user_wiki_page)
   else:
     new_wiki_page(data,r,token_comment,awarder,awardee,flair_count)
+  try:
+    tracker_page = r.get_wiki_page(data["running_subreddit"],"index/delta_tracker")
+    logging.debug("Found existing user wiki page")
+    tracker_found = True
+  except Exception as e:
+    if e.response.status_code == 404:
+      logging.debug("Did not find existing user wiki page")
+      tracker_found = False
+  if not tracker_found:
+    update_tracker_page(data,r,awardee)
+  else:
+    new_tracker_page(data,r,awardee)
 
 def new_wiki_page(data,r,token_comment,awarder,awardee,flair_count):
   submission_title = token_comment.submission.title
@@ -44,21 +55,31 @@ def new_wiki_page(data,r,token_comment,awarder,awardee,flair_count):
     initial_text = "/u/%s has received %s delta for the following comments:\n\n" % (awardee,flair_count)
   else:
     initial_text = "/u/%s has received %s deltas for the following comments:\n\n" % (awardee,flair_count)
-  add_title = "| Submission | Delta Comment | Awarded By | Date |\n| --- | :-: | --- | --- |\n"
+  add_header = "| Submission | Delta Comment | Awarded By | Date |\n| --- | :-: | --- | --- |\n"
   add_content = "|[%s](%s)|[Link](%s)|/u/%s|%s/%s/%s|\n" % (submission_title,submission_url,
         token_comment.permalink + "?context=2",awarder,today.month,today.day,today.year)
-  full_update = initial_text + add_title + add_content
+  full_update = initial_text + add_header + add_content
   r.edit_wiki_page(data["running_subreddit"],"user/" + awardee,full_update,"Created user's delta history page.")
-  tracker_page = r.get_wiki_page(data["running_subreddit"],"index/delta_tracker")
-  logging.debug("Found existing tracker wiki page")
-  logging.debug("Could not find existing tracker wiki page")
-  add_link = "* /u/%s - [Delta List](/r/%s/wiki/%s)" % (awardee,data["running_subreddit"],awardee)
-  r.edit_wiki_page(data["running_subreddit"],"delta_tracker",add_link,"Updated tracker")
 
-def update_wiki_page(data,r,token_comment,awarder,awardee,flair_count):
+def new_tracker_page(data,r,awardee):
+  initial_text = "Below is a list of all of the users that have earned deltas."
+  add_header = "| User | Delta List |\n| --- | --- |\n"
+  add_content = "|/u/%s|[Link](/r/%s/wiki/%s)" % (awardee,data["running_subreddit"],awardee)
+  full_update = initial_text + add_header + add_content
+  r.edit_wiki_page(data["running_subreddit"],"index/delta_tracker",full_update,"Updated tracker")
+
+def update_wiki_page(data,r,token_comment,awarder,awardee,flair_count,user_wiki_page):
   submission_title = token_comment.submission.title
   submission_url = token_comment.submission.permalink
   today = datetime.date.today()
+  if int(flair_count) < 2:
+    flair_text = "%s delta" % flair_count
+  else:
+    flair_text = "%s deltas" % flair_count
+  re.sub("(\d+) delta[s]?", flair_text)
+  print 
+
+def update_tracker_page(data,r,awardee):
   return
 
 # EOF
