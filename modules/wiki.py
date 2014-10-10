8a9f830484419bd7c385ba8d88428cf92709b55f
Pixel Orange <PixelOrange@users.noreply.github.com>
2014-10-10 05:15:14 -0500

    Update wiki.py

--- modules/wiki.py
+++ modules/wiki.py
@@ -87,7 +87,7 @@ def update_wiki_page(data,r,token_comment,awarder,awardee,flair_count,user_wiki_
   print("Printing table")
   print table
   new_content = '\n'.join(table)
-  full_update = initial_text + new content
+  full_update = initial_text + new_content
   r.edit_wiki_page(data["running_subreddit"],"user/" + awardee,full_update,"Updated user's delta history page.")
 
 def update_tracker_page(data,r,awardee):
