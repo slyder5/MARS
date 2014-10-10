0ad95f3bd991932dd20447085d1c8324097f93ff
Pixel Orange <PixelOrange@users.noreply.github.com>
2014-10-10 01:17:34 -0500

    Create testwiki.py

---
+++ modules/testwiki.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+# Import #
+##########
+
+import praw
+import logging
+from modules import account
+
+# Variables #
+#############
+
+data["running_subreddit"] = "PixelOrange"
+data["m_account"] = "1"
+r = account.start(data)
+
+# Functions #
+#############
+
+print data
+print r
