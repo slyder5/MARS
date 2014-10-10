0075a6899cfa2805aa5fe54ba88f40cf8ce1fece
Pixel Orange <PixelOrange@users.noreply.github.com>
2014-10-10 02:23:21 -0500

    Update wiki.py

--- modules/wiki.py
+++ modules/wiki.py
@@ -20,20 +20,14 @@ def start(data,r,token_comment,awarder,awardee,flair_count):
   submission_url = token_comment.submission.permalink
   submission_title = token_comment.submission.title
   today = datetime.date.today()
-  try:
-    user_wiki_page = r.get_wiki_page(data["running_subreddit"],"user/" + awardee)
+  user_wiki_page = r.get_wiki_page(data["running_subreddit"],"user/" + awardee)
+  if user_wiki_page:
     logging.debug("Found existing user wiki page")
-    if int(flair_count) < 2:
-      initial_text = "/u/%s has received %s delta for the following comments:\n\n" % (awardee,flair_count)
-    else:
-      initial_text = "/u/%s has received %s deltas for the following comments:\n\n" % (awardee,flair_count)
-    add_title = "| Submission | Delta Comment | Awarded By | Date |\n| --- | :-: | --- | --- |\n"
-    add_content = "|[%s](%s)|[Link](%s)|/u/%s|%s/%s/%s|\n" % (submission_title,submission_url,
-          token_comment.permalink + "?context=2",awarder,today.month,today.day,today.year)
-    full_update = initial_text + add_title + add_content
-    r.edit_wiki_page(data["running_subreddit"],"user/" + awardee,full_update,"Created user's delta history page.")
-  except:
+  else:
     logging.debug("Could not find existing user wiki page")
+
+def placeholder():
+    
     if int(flair_count) < 2:
       initial_text = "/u/%s has received %s delta for the following comments:\n\n" % (awardee,flair_count)
     else:
