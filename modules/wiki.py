381442b3cb48d724b72261c4f344b9805d11122a
Pixel Orange <PixelOrange@users.noreply.github.com>
2015-01-23 04:33:47 -0600

    Update wiki.py

--- modules/wiki.py
+++ modules/wiki.py
@@ -80,13 +80,13 @@ def update_wiki_page(data,r,token_comment,awarder,awardee,flair_count,user_wiki_
       if not re.match("(\| Submission |\| --- \|)",line):
         table.append(line)
     if re.match("Any delta history",line):
-      footnote = "\n" + line
+      footnote = line
     else:
       footnote = ""
   table.append(add_content)
   table.sort()
   new_content = '\n'.join(table)
-  full_update = initial_text + add_header + new_content + footnote
+  full_update = initial_text + add_header + new_content + "\n" + footnote
   r.edit_wiki_page(data["running_subreddit"],"user/" + awardee,full_update,"Updated user's delta history page.")
 
 def new_tracker_page(data,r,awardee,token_comment):
