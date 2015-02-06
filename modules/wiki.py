7378307b7cebfdb1a8ada97db4fba5e538a1d2b0
Pixel Orange <PixelOrange@users.noreply.github.com>
2015-02-06 16:43:13 -0600

    Reversed sorting

--- modules/wiki.py
+++ modules/wiki.py
@@ -83,7 +83,7 @@ def update_wiki_page(data,r,token_comment,awarder,awardee,flair_count,user_wiki_
     elif re.match("Any delta history",line):
       note = line + "\n\n"
   table.append(add_content)
-  table.sort()
+  table.sort(reverse=True)
   new_content = '\n'.join(table)
   full_update = initial_text + note + add_header + new_content
   r.edit_wiki_page(data["running_subreddit"],"user/" + awardee,full_update,"Updated user's delta history page.")
