24c8f1d3e50f5d6b0cc57cb737f6f0c80839cd73
Pixel Orange <PixelOrange@users.noreply.github.com>
2015-02-08 16:00:21 -0600

    Update wiki.py

--- modules/wiki.py
+++ modules/wiki.py
@@ -109,7 +109,7 @@ def update_tracker_page(data,r,awardee,token_comment,tracker_page):
   for line in lines:
     if re.match("(\|)",line):
       if not re.match("(\| User |\| --- \|)",line):
-        if not re.match(awardee,line):
+        if awardee not in line:
           table.append(line)
         else:
           table.append(add_content)
