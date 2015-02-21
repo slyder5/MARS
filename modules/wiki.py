a48fc32a98c60e33b5de8d1703e23520f514bbbd
Pixel Orange <PixelOrange@users.noreply.github.com>
2015-02-21 13:27:57 -0600

    Update wiki.py

--- modules/wiki.py
+++ modules/wiki.py
@@ -164,8 +164,8 @@ def new_queue_page(data,r,awardee,token_comment):
   token_comment_body = token_comment.body.replace("\n"," ")
   token_comment_body = token_comment_body.replace("&amp;","\&")
   add_content = "|/u/%s|[%s](%s)| [Approve](/message/compose/?to=%s&subject=%s&message=%s) / [Reject for Low Effort](/message/compose/?to=%s&subject=reject low effort&message=%s) / [Reject and Remind](/message/compose/?to=%s&subject=reject remind&message=%s) / [Reject for Abuse](/message/compose/?to=%s&subject=reject abuse&message=%s)|" % \
-  (awardee,token_comment_body,token_comment.permalink + "?context=2",data["running_username"],"approve",token_comment.permalink,
-  data["running_username"],token_comment.permalink)
+    (awardee,token_comment_body,token_comment.permalink + "?context=2",data["running_username"],"approve",token_comment.permalink,
+    data["running_username"],token_comment.permalink,data["running_username"],token_comment.permalink,data["running_username"],token_comment.permalink)
   full_update = initial_text + add_header + add_content
   r.edit_wiki_page(data["running_subreddit"],data["running_username"] + "/queue",full_update,"Updated queue")
 
@@ -176,7 +176,7 @@ def update_queue_page(data,r,awardee,token_comment,queue_page):
   token_comment_body = token_comment_body.replace("&amp;","\&")
   add_content = "|/u/%s|[%s](%s)| [Approve](/message/compose/?to=%s&subject=%s&message=%s) / [Reject for Low Effort](/message/compose/?to=%s&subject=reject low effort&message=%s) / [Reject and Remind](/message/compose/?to=%s&subject=reject remind&message=%s) / [Reject for Abuse](/message/compose/?to=%s&subject=reject abuse&message=%s)|" % \
     (awardee,token_comment_body,token_comment.permalink + "?context=2",data["running_username"],"approve",token_comment.permalink,
-    data["running_username"],token_comment.permalink)
+    data["running_username"],token_comment.permalink,data["running_username"],token_comment.permalink,data["running_username"],token_comment.permalink)
   old_content = queue_page.content_md
   lines = old_content.split("\n")
   table = []
