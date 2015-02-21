3dc4fee7309453442afae59bb28333fa2ac4abf7
Pixel Orange <PixelOrange@users.noreply.github.com>
2015-02-21 14:15:14 -0600

    Update wiki.py

--- modules/wiki.py
+++ modules/wiki.py
@@ -163,7 +163,7 @@ def new_queue_page(data,r,awardee,token_comment):
   add_header = "| Awardee | Comment | Action |\n| --- | --- | --- |\n"
   token_comment_body = token_comment.body.replace("\n"," ")
   token_comment_body = token_comment_body.replace("&amp;","\&")
-  add_content = "|/u/%s|[%s](%s)| [Approve](/message/compose/?to=%s&subject=%s&message=%s) / [Reject for Low Effort](/message/compose/?to=%s&subject=reject low effort&message=%s) / [Reject and Remind](/message/compose/?to=%s&subject=reject remind&message=%s) / [Reject for Abuse](/message/compose/?to=%s&subject=reject abuse&message=%s)|" % \
+  add_content = "|/u/%s|[%s](%s)| [Approve](/message/compose/?to=%s&subject=%s&message=%s) \n\n [Reject for Low Effort](/message/compose/?to=%s&subject=reject low effort&message=%s) \n\n [Reject and Remind](/message/compose/?to=%s&subject=reject remind&message=%s) \n\n [Reject for Abuse](/message/compose/?to=%s&subject=reject abuse&message=%s)|" % \
     (awardee,token_comment_body,token_comment.permalink + "?context=2",data["running_username"],"approve",token_comment.permalink,
     data["running_username"],token_comment.permalink,data["running_username"],token_comment.permalink,data["running_username"],token_comment.permalink)
   full_update = initial_text + add_header + add_content
@@ -174,7 +174,7 @@ def update_queue_page(data,r,awardee,token_comment,queue_page):
   add_header = "| Awardee | Comment | Action |\n| --- | --- | --- |\n"
   token_comment_body = token_comment.body.replace("\n"," ")
   token_comment_body = token_comment_body.replace("&amp;","\&")
-  add_content = "|/u/%s|[%s](%s)| [Approve](/message/compose/?to=%s&subject=%s&message=%s) / [Reject for Low Effort](/message/compose/?to=%s&subject=reject low effort&message=%s) / [Reject and Remind](/message/compose/?to=%s&subject=reject remind&message=%s) / [Reject for Abuse](/message/compose/?to=%s&subject=reject abuse&message=%s)|" % \
+  add_content = "|/u/%s|[%s](%s)| [Approve](/message/compose/?to=%s&subject=%s&message=%s) \n\n [Reject for Low Effort](/message/compose/?to=%s&subject=reject low effort&message=%s) \n\n [Reject and Remind](/message/compose/?to=%s&subject=reject remind&message=%s) \n\n [Reject for Abuse](/message/compose/?to=%s&subject=reject abuse&message=%s)|" % \
     (awardee,token_comment_body,token_comment.permalink + "?context=2",data["running_username"],"approve",token_comment.permalink,
     data["running_username"],token_comment.permalink,data["running_username"],token_comment.permalink,data["running_username"],token_comment.permalink)
   old_content = queue_page.content_md
