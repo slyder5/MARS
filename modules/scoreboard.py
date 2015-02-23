a9dedac08feca4dbc9989a4b2ea7021bfbf157a5
Pixel Orange <PixelOrange@users.noreply.github.com>
2015-02-22 21:15:09 -0600

    Update scoreboard.py

--- modules/scoreboard.py
+++ modules/scoreboard.py
@@ -1,1 +1,22 @@
+# encoding: utf-8
 
+# Comments #
+############
+
+# Import #
+##########
+
+import praw
+import logging
+import time
+import datetime
+import calendar
+from urllib2 import HTTPError
+import re
+
+# Functions #
+#############
+
+def start(data,r):
+  logging.debug("Starting Module: Scoreboard")
+  
