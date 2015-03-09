a5384ec88865796ff2216a66b60acf3e8ddef26a
Pixel Orange <PixelOrange@users.noreply.github.com>
2015-03-08 20:45:17 -0500

    Update token.py

--- modules/token.py
+++ modules/token.py
@@ -9,30 +9,33 @@
 import praw
 import logging
 import re
+import messages
 
 # Functions #
 #############
 
-def start_increment(data,r,awardee):
+def start_increment(data,msg,r,awardee):
   logging.debug("Starting Module: Token - Increment")
-  old_flair,old_count = get_flair(data,r,awardee)
+  old_flair,old_count = get_flair(data,msg,r,awardee)
   new_flair,new_count = increment_flair(old_flair,old_count)
-  set_flair(data,r,awardee,new_flair)
+  set_flair(data,msg,r,awardee,new_flair)
   return new_count
 
-def start_decrement(data,r,awardee):
+def start_decrement(data,msg,r,awardee):
   logging.debug("Starting Module: Token - Decrement")
-  old_flair,old_count = get_flair(data,r,awardee)
+  old_flair,old_count = get_flair(data,msg,r,awardee)
   new_flair,new_count = decrement_flair(old_flair,old_count)
-  set_flair(data,r,awardee,new_flair)
+  set_flair(data,msg,r,awardee,new_flair)
   return new_count
 
-def get_flair(data,r,awardee):
+def get_flair(data,msg,r,awardee):
   logging.debug("Getting the awardee's flair")
   awardee_flair = r.get_flair(data["running_subreddit"],awardee)
   if awardee_flair["flair_text"] == None:
     flair_count = 0
-    r.send_message(awardee,msg["congrats_first_subject"],msg["congrats_first_body"] % (data["running_subreddit"],awardee))
+    congrats_first_subject = messages.congrats_first_subject(msg)
+    congrats_first_body = messages.congrats_first_body(msg)
+    r.send_message(awardee,congrats_first_subject,congrats_first_body)
   else:
     flair_count = re.search('(\d+)', awardee_flair["flair_text"])
     if flair_count.group(0):
@@ -52,7 +55,7 @@ def decrement_flair(flair,old_count):
   flair["flair_text"] = str(new_count) + "\u2206"
   return (flair,new_count)
 
-def set_flair(data,r,awardee,awardee_flair):
+def set_flair(data,msg,r,awardee,awardee_flair):
   logging.debug("Setting the awardee's new flair")
   r.set_flair(data["running_subreddit"],awardee,awardee_flair["flair_text"])
 
