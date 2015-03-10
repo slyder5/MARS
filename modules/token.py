4dd33cdc5520a0a345d33baaaa5f4c3cb79ee2f0
Pixel Orange <PixelOrange@users.noreply.github.com>
2015-03-10 09:07:33 -0500

    Update token.py

--- modules/token.py
+++ modules/token.py
@@ -32,9 +32,13 @@ def get_flair(data,msg,r,awardee):
   logging.debug("Getting the awardee's flair")
   awardee_flair = r.get_flair(data["running_subreddit"],awardee)
   if awardee_flair["flair_text"] == None:
+    logging.debug("New Awardee")
     flair_count = 0
+    logging.debug("Getting Congrats Subject")
     congrats_first_subject = messages.congrats_first_subject(msg)
+    logging.debug("Getting Congrats Body")
     congrats_first_body = messages.congrats_first_body(data,awardee)
+    logging.debug("Sending Congrats Mail")
     r.send_message(awardee,congrats_first_subject,congrats_first_body)
   else:
     flair_count = re.search('(\d+)', awardee_flair["flair_text"])
